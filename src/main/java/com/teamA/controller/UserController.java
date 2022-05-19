deefff667df301a31d99002d10bfd995cf41555f
Gudrun Vene <gudrun.vene@gmail.com>
2022-05-19 09:16:05 +0300

    git problems with merging

--- src/main/java/com/teamA/controller/UserController.java
+++ src/main/java/com/teamA/controller/UserController.java
@@ -1,16 +1,23 @@
 package com.teamA.controller;
 
 import com.teamA.model.User;
+import com.teamA.repository.UserRepository;
 import com.teamA.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private UserRepository userRepository;
+
     @Autowired
     private UserService userService;
 
